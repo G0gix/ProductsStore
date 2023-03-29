3b6f4fdfb71ba5e04b24aa1125764a403845bfa8
G0gix <jora.grintsevich@gmail.com>
2023-03-29 14:54:34 +0500

    Create CategoryWithFields And FieldModel

---
+++ ProductsStore/Models/MainModels/CategoryWithFields.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ProductsStore.Models.MainModels
+{
+    public class CategoryWithFields
+    {
+        public CategoryModel Category { get; set; }
+        public List<FieldModel> Fields { get; set; }
+    }
+
+    public class FieldModel
+    {
+        public string Name { get; set; }
+    }
+}
