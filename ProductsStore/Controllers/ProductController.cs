d76b774a6bebd606e8d4fb992916f3990aef22f0
G0gix <jora.grintsevich@gmail.com>
2023-03-29 17:15:46 +0500

    Create ProductController

---
+++ ProductsStore/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsStore.Models.dbContext;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            using (HardCodeContext db = new HardCodeContext())
+            {
+                List<Product> products =  await db.Products.ToListAsync();
+
+                if (products.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(products);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            using (HardCodeContext db = new HardCodeContext())
+            {
+                Product product = await db.Products.FindAsync(id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(product);
+            }
+        }
+    }
+}
